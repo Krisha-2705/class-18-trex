1b92754efa9dbef9a77467ed8b312e660fca776a
Krisha-2705 <145978458+Krisha-2705@users.noreply.github.com>
2024-01-08 15:12:41 +1100

    Add files via upload

---
+++ PRO-C18-v3-Trex-color.github.io-main/Pro-c18-sa1-template_code-main/sketch.js
@@ -0,0 +1,12 @@
+var s1
+function setup() {
+  createCanvas(400,400);
+s1= new Student ("Krisha",14,"eating")
+}
+
+function draw() 
+{
+  s1.show()
+  background(30);
+  
+}
---
+++ PRO-C18-v3-Trex-color.github.io-main/Pro-c18-sa1-template_code-main/student.js
@@ -0,0 +1,15 @@
+
+class Student
+{ constructor(name,age,hobby,) {
+this.name=name;
+this.hobby=hobby;
+this.age=age;
+}
+show(){
+console.log (this.name,this.age,this.hobby);
+}
+   
+}
+
+
+
